f7b35ec64d1b801a7c9d0da8459d0542b91294f0
Tony.li <tony.li@gaiaworks.cn>
2020-07-30 10:28:47 +0800

    添加BaseActivity和BaseFragment

---
+++ app/src/main/java/com/wanandroid/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.wanandroid.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+abstract class BaseFragment : androidx.fragment.app.Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(getLayoutResId(), container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initView()
+        initData()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    abstract fun getLayoutResId(): Int
+
+    abstract fun initView()
+
+    abstract fun initData()
+}
