a53d82be7dc9e0d0a4e9a4815ec84e3374229347
Tony.li <tony.li@gaiaworks.cn>
2020-07-30 10:32:40 +0800

    添加BottomNavigationView界面与相关逻辑

--- app/src/main/java/com/wanandroid/MainActivity.kt
+++ app/src/main/java/com/wanandroid/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.wanandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.wanandroid.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    override fun getLayoutResId() = R.layout.activity_main
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+    override fun initView() {
+        bottomNavigationView = findViewById(R.id.bnv_view)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment
+        val navController = host.navController
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun initData() {
     }
 }
