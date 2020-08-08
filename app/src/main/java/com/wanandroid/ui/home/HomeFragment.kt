15d37d264169e5a3c3649bbb2d3bb4e6dd6cafe4
Donkey <1600350642@qq.com>
2020-08-08 10:35:15 +0800

    添加布局和ViewPager、TabLayout主界面切换逻辑

--- app/src/main/java/com/wanandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wanandroid/ui/home/HomeFragment.kt
@@ -1,42 +1,74 @@
 package com.wanandroid.ui.home
 
-import android.util.Log
-import android.widget.Toast
-import com.wanandroid.App
-import com.wanandroid.NetWorkUtils
+import androidx.fragment.app.Fragment
 import com.wanandroid.R
 import com.wanandroid.base.BaseFragment
-import com.wanandroid.model.http.ResponseResult
-import com.wanandroid.model.repository.LoginRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.wanandroid.ui.square.SquareFragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import com.wanandroid.ui.first.FirstFragment
+import com.wanandroid.ui.guide.GuideFragment
+import com.wanandroid.ui.lastedProject.LastedProjectFragment
+import com.wanandroid.ui.system.SystemFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * Created by Donkey
  * on 3:18 PM
  */
-@Suppress("IMPLICIT_CAST_TO_ANY")
-class HomeFragment : BaseFragment()   {
-    override fun getLayoutResId(): Int = R.layout.fragment_home
+class HomeFragment : BaseFragment() {
+    private val titleList  = arrayListOf("首页","导航","项目", "广场", "体系")
+    private val fragmentList = arrayListOf<Fragment>()
+    private val firstFragment by lazy { FirstFragment() } //首页
+    private val guideFragment by lazy { GuideFragment() } //导航
+    private val lastedProjectFragment by lazy { LastedProjectFragment() } // 最新项目
+    private val squareFragment by lazy { SquareFragment() } // 广场
+    private val systemFragment by lazy { SystemFragment() } // 体系
+    private var mOnPageChangeCallback: ViewPager2.OnPageChangeCallback? = null
+
+    init {
+        fragmentList.add(firstFragment)
+        fragmentList.add(guideFragment)
+        fragmentList.add(lastedProjectFragment)
+        fragmentList.add(squareFragment)
+        fragmentList.add(systemFragment)
+    }
+
+    override fun getLayoutResId() = R.layout.fragment_home
 
     private val  TAG : String = HomeFragment::class.java.simpleName
     override fun initView() {
-        Log.d(TAG, "initView")
-        val loginRepository =  LoginRepository()
+        initViewPager()
+    }
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val result =  withContext(Dispatchers.IO){loginRepository.login("1600350642","123456") }
-            if (result is ResponseResult.Success) {
-                Log.d(TAG, result.data.toString())
-                Toast.makeText(App.getContext(),"登录成功",Toast.LENGTH_SHORT).show()
-            } else if (result is ResponseResult.Error) {
-                Log.d(TAG, result.errorMsg)
-                Toast.makeText(App.getContext(),result.errorMsg,Toast.LENGTH_SHORT).show()
-            }
+
+    private fun initViewPager() {
+        viewPager.offscreenPageLimit = 1
+        viewPager.adapter =  object : FragmentStateAdapter(this) {
+            override fun createFragment(position: Int) = fragmentList[position]
+
+            override fun getItemCount() = titleList.size
         }
 
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = titleList[position]
+        }.attach()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mOnPageChangeCallback == null) mOnPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+
+            }
+        }
+        mOnPageChangeCallback?.let { viewPager.registerOnPageChangeCallback(it) }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mOnPageChangeCallback?.let { viewPager.unregisterOnPageChangeCallback(it) }
     }
 
     override fun initData() {
