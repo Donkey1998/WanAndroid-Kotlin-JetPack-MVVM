3d77ec1253ac4cd7d549dbac8eb540f082bba814
Donkey <1600350642@qq.com>
2020-08-08 10:35:45 +0800

    添加相关的Fragment

---
+++ app/src/main/java/com/wanandroid/ui/guide/ProjectFragment.kt
@@ -0,0 +1,17 @@
+package com.wanandroid.ui.guide
+import com.wanandroid.R
+import com.wanandroid.base.BaseFragment
+
+/**
+ * Created by Donkey
+ * on 3:18 PM
+ */
+class GuideFragment : BaseFragment() {
+    override fun getLayoutResId() = R.layout.fragment_guide
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
---
+++ app/src/main/java/com/wanandroid/ui/square/SquareFragment.kt
@@ -0,0 +1,19 @@
+package com.wanandroid.ui.square
+
+import android.util.Log
+import com.wanandroid.R
+import com.wanandroid.base.BaseFragment
+
+/**
+ * Created by Donkey
+ * on 3:18 PM
+ */
+class SquareFragment : BaseFragment() {
+    override fun getLayoutResId() = R.layout.fragment_square
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
