9d9b2f633899bd182c85fc6e8301fddc558a979f
Donkey <1600350642@qq.com>
2020-08-22 10:27:43 +0800

    引入X5WebView

--- app/src/main/java/com/wanandroid/binding/BindingAdapters.kt
+++ app/src/main/java/com/wanandroid/binding/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.wanandroid.binding
 import android.os.Build
 import android.text.Html
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
@@ -23,4 +24,11 @@ fun SwipeRefreshLayout.onRefresh(action: () -> Unit) {
 @BindingAdapter("htmlText")
 fun bindHtmlText(view: TextView, html:String){
     view.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY) else Html.fromHtml(html)
+}
+
+@BindingAdapter("title", "icon", "navigationClick", requireAll = false)
+fun Toolbar.init(titleResId: Int, iconResId: Int, action: () -> Unit) {
+    setTitle(titleResId)
+    setNavigationIcon(iconResId)
+    setNavigationOnClickListener { action() }
 }
