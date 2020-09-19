0d5b04c6923294b847eda4a899a110208e628e04
Donkey <1600350642@qq.com>
2020-09-19 14:04:18 +0800

    我的页面布局搭建

--- app/src/main/java/com/wanandroid/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/wanandroid/ui/profile/ProfileFragment.kt
@@ -1,7 +1,9 @@
 package com.wanandroid.ui.profile
 
+import android.util.Log
 import com.wanandroid.R
 import com.wanandroid.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * Created by Donkey
@@ -11,8 +13,34 @@ class ProfileFragment : BaseFragment() {
     override fun getLayoutResId() = R.layout.fragment_profile
 
     override fun initView() {
+        setOnClickListener()
     }
 
     override fun initData() {
+
+    }
+
+    private fun setOnClickListener(){
+        login.setOnClickListener {
+            Log.d("ProfileFragment", "login")
+        }
+        header.setOnClickListener {
+            Log.d("ProfileFragment", "header")
+        }
+        readHistory.setOnClickListener {
+            Log.d("ProfileFragment", "readHistory")
+        }
+        clearCache.setOnClickListener {
+            Log.d("ProfileFragment", "clearCache")
+        }
+        aboutAuthor.setOnClickListener {
+            Log.d("ProfileFragment", "aboutAuthor")
+        }
+        systemSetting.setOnClickListener {
+            Log.d("ProfileFragment", "systemSetting")
+        }
+
+
+
     }
 }
