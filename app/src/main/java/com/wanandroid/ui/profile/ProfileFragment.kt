0bc2f45e388bab1e17828ae59f01566dde6cd8b1
Donkey <1600350642@qq.com>
2020-09-21 18:47:32 +0800

    注册登录页面1.0版本搭建

--- app/src/main/java/com/wanandroid/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/wanandroid/ui/profile/ProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.wanandroid.ui.profile
 
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import com.wanandroid.App
 import com.wanandroid.R
 import com.wanandroid.base.BaseFragment
+import com.wanandroid.ui.login.RegisterLoginActivity
 import kotlinx.android.synthetic.main.fragment_profile.*
+
 
 /**
  * Created by Donkey
  * on 3:18 PM
  */
+@Suppress("DEPRECATED_IDENTITY_EQUALS")
 class ProfileFragment : BaseFragment() {
     override fun getLayoutResId() = R.layout.fragment_profile
 
@@ -22,7 +29,8 @@ class ProfileFragment : BaseFragment() {
 
     private fun setOnClickListener(){
         login.setOnClickListener {
-            Log.d("ProfileFragment", "login")
+            val intent = Intent(App.getContext(), RegisterLoginActivity::class.java)
+            startActivityForResult(intent, 1) // requestCode -> 666
         }
         header.setOnClickListener {
             Log.d("ProfileFragment", "header")
@@ -40,7 +48,17 @@ class ProfileFragment : BaseFragment() {
             Log.d("ProfileFragment", "systemSetting")
         }
 
+    }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode === 2) {
+            if (requestCode === 1) {
+                val userName = data?.getStringExtra("userName")
+                val userId = data?.getStringExtra("userId")
+                loginRegister.text = userName
+            }
+        }
     }
 }
+
